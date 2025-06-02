04740867c6d80933fb503212dd6b47760a58224e
Islam Atwa <islamatwa429@gmail.com.com>
2025-06-03 00:31:00 +0300

    NavBar

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import Bootstrap CSS 
 import 'bootstrap/dist/css/bootstrap.min.css';
+// Import Bootstrap JS 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
